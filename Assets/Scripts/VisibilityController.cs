f4b8266945ef7a4a0e664e0a76759d157fba5783
João Victor Sena <jvsena16@gmail.com>
2021-05-22 10:44:19 -0300

    [NEW]
    Visibility controller

---
+++ Assets/Scripts/VisibilityController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibilityController : MonoBehaviour
+{
+     public void showScreen(GameObject screen){
+        screen.gameObject.SetActive(true);
+    }
+
+    public void hideScreen(GameObject screen){
+        screen.gameObject.SetActive(false);
+    }
+}
